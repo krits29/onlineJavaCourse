7359a7b3d1c376fb14e1b7a3228bf4de04c2fe97
krits29 <31025768+krits29@users.noreply.github.com>
2019-07-29 11:42:44 -0700

    simple Hello World

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**
+ * Created by kritisharma on 7/23/19.
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!!");
+
+        int MyFirstNumber = (10+5) + (2*10);
+        int MySecondNumber = 12;
+        int MyThirdNumber = 6;
+
+        int MyTotal = MyFirstNumber + MySecondNumber + MyThirdNumber;
+
+        int MyLastNumber = 1000 - MyTotal;
+
+        System.out.println(MyTotal);
+    }
+}
