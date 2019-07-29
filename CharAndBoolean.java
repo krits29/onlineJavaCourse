b183e17f40bc8f25be9767d5da7a8b01c9b5fa28
krits29 <31025768+krits29@users.noreply.github.com>
2019-07-29 11:35:57 -0700

    datatypes: char and boolean

---
+++ CharAndBoolean.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class CharAndBoolean
+{
+    public static void main(String[] args)
+    {
+	    char c = 'C';
+        char uniCode = '\u0044'; //go to the uni code website and look at row and column
+        System.out.println(c);   //https://unicode-table.com/en/
+        System.out.println(uniCode);
+        char copyright = '\u00A9'; // to get cool symbols and other languages
+        System.out.println(copyright);
+
+        boolean trueVal = true;
+        boolean falseVal = false;
+
+        boolean isCustomerOver21 = true;
+
+
+    }
+}
