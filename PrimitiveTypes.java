d035f02a981e549e23b45256f0ee7a6fe222dd6f
krits29 <31025768+krits29@users.noreply.github.com>
2019-07-29 11:41:43 -0700

    datatypes: long, short, int, byte

---
+++ PrimitiveTypes.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class PrimitiveTypes
+{
+    public static void main(String[] args)
+    {
+        byte b = 10;
+        short s = 12;
+        int i = 29;
+
+        long total = 50000L +  (10L * (b + s + i));
+        System.out.println("Total = " + total);
+
+    }
+}
