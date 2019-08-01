64e5fe19e1d3febaa497967db78cc22ad577d3a6
krits29 <31025768+krits29@users.noreply.github.com>
2019-08-01 12:46:43 -0700

    some code to show Mumma what I've learned so far

---
+++ HelloMumma.java
@@ -0,0 +1,25 @@
+  GNU nano 2.0.6                       New Buffer                                            Modified  
+
+public class HelloMumma
+{ 
+        public static void main(String[] args)
+        {
+                System.out.println("Hello Mumma, can you smile for me?");
+
+                boolean areYouSmiling = true;
+
+                if (areYouSmiling == true)
+                {
+                        System.out.println(":)" );
+                }
+                else if (areYouSmiling == false)
+                {
+                        System.out.println("Smile please");
+                }
+                else
+                {
+                        System.out.println("Somthing went wrong");
+                }
+        }
+}
+
