f375d1e99ff9b04dd96fb4cde4e601b160107041
krits29 <31025768+krits29@users.noreply.github.com>
2019-08-01 12:43:32 -0700

    Methods Coding Exercise #6

---
+++ EqualSumChecker.java
@@ -0,0 +1,28 @@
+/**
+ * Created by kritisharma on 8/1/19.
+ */
+
+
+public class EqualSumChecker
+{
+    public static boolean hasEqualSum(int first, int second, int sum)
+    {
+        return (first + second == sum);
+    }
+
+   /* public static void main(String[] args)
+    {
+        System.out.println(hasEqualSum(2, 3, 5));
+    }*/
+}
+
+/* THIS IS MY CODE, the above code is Papa's
+public class EqualSumChecker
+{
+    public static boolean hasEqualSum(int first, int second, int sum)
+    {
+
+        return (first + second == sum);
+
+    }
+}*/
---
+++ EqualSumCheckerTest.java
@@ -0,0 +1,7 @@
+public class EqualSumCheckerTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(EqualSumChecker.hasEqualSum(3, 2, 5));
+    }
+}
