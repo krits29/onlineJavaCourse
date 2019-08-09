470997803ad3b7cdf9cce555de5ac78e22b01c5f
krits29 <31025768+krits29@users.noreply.github.com>
2019-08-09 11:50:40 -0700

    Overloading Methods Practice

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -10,6 +10,9 @@ public class MethodOverloading
                             // this is called an overloaded method
         calcFeetAndInchesToCentimeters(6, 0);
         calcFeetAndInchesToCentimeters(72);
+
+        getDurationString(5, 30);
+        getDurationString(350);
     }
 
     public static int CalculateScore ( String playerName, int score)
@@ -23,6 +26,8 @@ public class MethodOverloading
         System.out.println("Unnamed player scored " + score + " points.");
         return score * 1000;
     }
+
+    /////---challenge---/////
 
     public static double calcFeetAndInchesToCentimeters(int feet, int inches)
     {
@@ -52,6 +57,32 @@ public class MethodOverloading
         {
             return -1;
         }
+    }
+
+    //---another challenge---//
+
+    public static void getDurationString(int minutes, int seconds)
+    {
+        if (minutes<0 || seconds > 59 || seconds < 0)
+        {
+            System.out.println("invalid value");
+        }
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        System.out.println(hours + " hrs " + minutes + " mins " + seconds + " secs" );
+    }
+
+    public static void getDurationString(int seconds)
+    {
+        if (seconds < 0)
+        {
+            System.out.println("invalid value");
+        }
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        getDurationString(minutes, remainingSeconds);
     }
 
 }
