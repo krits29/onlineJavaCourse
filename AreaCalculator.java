c72ea50039f0c05b1bdd16603c4d77c46272fb7f
krits29 <31025768+krits29@users.noreply.github.com>
2019-08-09 13:08:43 -0700

    Overloading Coding Exercise #1

---
+++ AreaCalculator.java
@@ -0,0 +1,27 @@
+/**
+ * Created by kritisharma on 8/9/19.
+ */
+public class AreaCalculator
+{
+    public static double area (double radius)
+    {
+        if (radius < 0)
+        {
+            System.out.println("invalid value");
+            return -1;
+        }
+
+        return (Math.PI * radius * radius);
+    }
+
+    public static double area (double x, double y)
+    {
+        if (x < 0 || y < 0)
+        {
+            System.out.println("invalid value");
+            return -1;
+        }
+
+        return (x * y);
+    }
+}
