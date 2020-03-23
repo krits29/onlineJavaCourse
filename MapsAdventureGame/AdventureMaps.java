9c212c9de32fb6196605e5f8d4fae9dd88af195b
krits29 <31025768+krits29@users.noreply.github.com>
2020-03-23 13:58:44 -0700

    Maps Coding Exercise #1

--- MapsAdventureGame/AdventureMaps.java
+++ MapsAdventureGame/AdventureMaps.java
@@ -13,28 +13,40 @@ public class AdventureMaps
     {
         Scanner scan = new Scanner(System.in);
 
-        locations.put(0, new Locations(0, "You are sitting in the front of a computer learning java"));
-        locations.put(1, new Locations(1, "You are inside your bathroom taking a long shower"));
-        locations.put(2, new Locations(2, "You are playing tennis in your regular Sunday tennis class"));
-        locations.put(3, new Locations(3, "You are taking a test in your AP Chemistry class"));
-        locations.put(4, new Locations(4, "You are in the movie theater with your best friends"));
-        locations.put(5, new Locations(5, "You are locked in the garage for your bad behavior"));
+        Map<String, Integer> temporaryExit = new HashMap<String, Integer>();
+        locations.put(0, new Locations(0, "You are sitting in the front of a computer learning java", temporaryExit));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
+        temporaryExit = new HashMap<String, Integer>();
+        temporaryExit.put("W", 2);
+        temporaryExit.put("E", 3);
+        temporaryExit.put("S", 4);
+        temporaryExit.put("N", 5);
+        locations.put(1, new Locations(1, "You are inside your bathroom taking a long shower", temporaryExit));
 
-        locations.get(2).addExit("N", 5);
+        temporaryExit = new HashMap<String, Integer>();
+        temporaryExit.put("N", 5);
+        locations.put(2, new Locations(2, "You are playing tennis in your regular Sunday tennis class", temporaryExit));
 
-        locations.get(3).addExit("W", 1);
+        temporaryExit = new HashMap<String, Integer>();
+        temporaryExit.put("W", 1);
+        locations.put(3, new Locations(3, "You are taking a test in your AP Chemistry class", temporaryExit));
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
+        temporaryExit = new HashMap<String, Integer>();
+        temporaryExit.put("N", 1);
+        temporaryExit.put("W", 2);
+        locations.put(4, new Locations(4, "You are in the movie theater with your best friends", temporaryExit));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
+        temporaryExit = new HashMap<String, Integer>();
+        temporaryExit.put("S", 1);
+        temporaryExit.put("W", 2);  //locations.get(5).addExit("W", 2);
+        locations.put(5, new Locations(5, "You are locked in the garage for your bad behavior", temporaryExit));
 
+        Map<String, String> vocab = new HashMap<String, String>();
+        vocab.put("QUIT", "Q");
+        vocab.put("NORTH", "N");
+        vocab.put("SOUTH", "S");
+        vocab.put("EAST", "E");
+        vocab.put("WEST", "W");
 
         int loc = 1;
         while(true)
@@ -46,7 +58,7 @@ public class AdventureMaps
             }
 
             Map<String, Integer> exits = locations.get(loc).getExits();   //indicating directions now
-            System.out.print("Avaiable exits are ");
+            System.out.print("Available exits are ");
             for(String exit: exits.keySet())
             {
                 System.out.print(exit + ", ");
@@ -54,6 +66,18 @@ public class AdventureMaps
             System.out.println();
 
             String directions = scan.nextLine().toUpperCase();
+            if(directions.length() > 1)
+            {
+                String[] words = directions.split(" ");
+                for(String word : words)
+                {
+                    if(vocab.containsKey(word))
+                    {
+                        directions = vocab.get(word);
+                        break;
+                    }
+                }
+            }
 
             if(exits.containsKey(directions))
             {
@@ -65,6 +89,20 @@ public class AdventureMaps
             }
 
         }
+
+//        String[] road = "You are standing at the end of a road".split(" ");   //split up by delimeter which is the space in this case
+//        for(String i : road)
+//        {
+//            System.out.println(i);
+//        }
+//
+//        System.out.println("- - - - - - - - - - -");
+//
+//        String[] building = "You are inside a building, wow thats cool".split(", ");
+//        for(String i : building)
+//        {
+//            System.out.println(i);
+//        }
 
     }
 }
--- MapsAdventureGame/Locations.java
+++ MapsAdventureGame/Locations.java
@@ -10,18 +10,25 @@ public class Locations
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Locations(int locationID, String description)
+    public Locations(int locationID, String description, Map<String, Integer> exits)
     {
         LocationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        if(exits != null)
+        {
+            this.exits = new HashMap<String, Integer>(exits);
+        }
+        else
+        {
+            this.exits = new HashMap<String, Integer>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location)
-    {
-        exits.put(direction, location);   //map: key, value
-    }
+//    public void addExit(String direction, int location)
+//    {
+//        exits.put(direction, location);   //map: key, value
+//    }
 
     public int getLocationID()
     {
@@ -35,7 +42,7 @@ public class Locations
 
     public Map<String, Integer> getExits()
     {
-        return new HashMap<String, Integer>(exits);  //instead of returnign the exits, we can create a new map and pass the exits
+        return new HashMap<String, Integer>(exits);  //instead of returning the exits, we can create a new map and pass the exits
     }                                                //just a copy so that it cannot be changed at all
 
 
