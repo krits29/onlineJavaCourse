e81a22e91d34dd9583e0fa87b46ef86e87d58be1
krits29 <31025768+krits29@users.noreply.github.com>
2020-03-27 15:49:49 -0700

    Using sorted collections

---
+++ SortedCollections/StockItem.java
@@ -0,0 +1,95 @@
+/**
+ * Created by kritisharma on 3/27/20.
+ */
+public class StockItem implements Comparable<StockItem>
+{
+    private final String name;
+    private double price;
+    private int quantityStock;
+
+    public StockItem(String name, double price)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        if(price > 0)
+        {
+            this.price = price;
+        }
+    }
+
+    public int quantityInStock()
+    {
+        return quantityStock;
+    }
+
+    public void adjustStock(int quantityStock)
+    {
+        int newQuantity = this.quantityStock + quantityStock;
+        if(newQuantity >= 0)
+        {
+            this.quantityStock = quantityStock;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        System.out.println("StockItem.equals");
+        if(obj == this)
+        {
+            return true;  //its the same if you are comparig to itself
+        }
+
+        if((obj == null) || (obj.getClass() != this.getClass()))
+        {
+            return false;  //cant be null or a different class
+        }
+
+        String objName = ((StockItem) obj).getName();  //casting it as stockitem
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return this.name.hashCode() + 31; // using a arbirtaty number
+    }
+
+    @Override
+    public int compareTo(StockItem o)
+    {
+        System.out.println("Entering StockItem.compareTo");
+        if(this == o)
+        {
+            return 0;
+        }
+
+        if(o != null)
+        {
+            return this.name.compareTo(o.getName());
+        }
+
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.name + " : Price $" + getPrice();
+    }
+}
---
+++ SortedCollections/StockList.java
@@ -0,0 +1,75 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kritisharma on 3/27/20.
+ */
+public class StockList
+{
+    private final Map<String, StockItem> list;
+
+    public StockList()
+    {
+        this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item)
+    {
+        if (item != null)
+        {
+            StockItem inStock = list.getOrDefault(item.getName(), item); //.getOrDefault saves an extra test for null
+            //check if it is already in the stock
+            if (inStock != item)
+            {
+                item.adjustStock(inStock.quantityInStock());
+            }
+
+            list.put(item.getName(), item);
+
+            return item.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity)
+    {
+        StockItem inStock = list.getOrDefault(item, null);
+
+        if((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity > 0))
+        {
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+
+        return 0;
+    }
+
+    public StockItem get(String key)
+    {
+        return list.get(key);
+    }
+
+    public Map<String, StockItem> Items()
+    {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString()
+    {
+        String str = "\n Stock List\n";
+        double totalCost = 0.0;
+        for(Map.Entry<String, StockItem> item :list.entrySet() )
+        {
+            StockItem stockItem = item.getValue();
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+
+            str = str + stockItem + ", There are " + stockItem.quantityInStock() + " in stock.";
+            str = str + "Value of item " + itemValue;
+            totalCost += itemValue;
+        }
+
+        return str + "Total stock value: " + totalCost;
+    }
+}
